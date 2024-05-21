6e15c2e44f9bc8839ec1eb03cb9b0a71d93366a3
kanoha9798 <km.nick34@gmail.com>
2024-05-21 19:48:48 +0900

    fizzbuzz問題提出1回目

---
+++ 01.fizzbuzz/fizzbuzz.rb
@@ -0,0 +1,16 @@
+#!/usr/bin/env ruby
+
+(1..20).each do |number|
+  case 
+  when number % 3 == 0 && number % 5 == 0
+    puts "fizzbuzz"
+  when number % 3 == 0
+    puts "fizz"
+  when number % 5 == 0
+    puts "buzz"
+  else
+    puts number
+  end
+  end
+
+
