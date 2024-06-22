86b4179fbb8327e82f1c902030409f0091a3e09a
kanoha9798 <km.nick34@gmail.com>
2024-06-22 13:41:04 +0900

    2回目の提出

--- 01.fizzbuzz/fizzbuzz.rb
+++ 01.fizzbuzz/fizzbuzz.rb
@@ -1,16 +1,15 @@
 #!/usr/bin/env ruby
 
 (1..20).each do |number|
-  case 
-  when number % 3 == 0 && number % 5 == 0
-    puts "fizzbuzz"
-  when number % 3 == 0
-    puts "fizz"
-  when number % 5 == 0
-    puts "buzz"
+  if number % 3 == 0 && number % 5 == 0
+    puts "Fizzbuzz"
+  elsif number % 3 == 0
+    puts "Fizz"
+  elsif number % 5 == 0
+    puts "Buzz"
   else
     puts number
   end
   end
 
-#1回目の提出
+#2回目の提出
