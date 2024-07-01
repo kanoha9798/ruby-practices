a33aac3fabe40bcc49cb8356402c08a6a8e3346d
kanoha9798 <km.nick34@gmail.com>
2024-07-01 21:07:12 +0900

    3回目の提出

--- 02.calendar/cal.rb
+++ 02.calendar/cal.rb
@@ -7,8 +7,8 @@ opt = OptionParser.new
 
 params = {}
 
-opt.on('-y year') {|year| params[:y] = year.to_i}
-opt.on('-m month') {|month| params[:m] = month.to_i}
+opt.on('-y year') { |year| params[:y] = year.to_i }
+opt.on('-m month') { |month| params[:m] = month.to_i }
 
 opt.parse!(ARGV)
 
@@ -20,22 +20,17 @@ start_date = Date.new(year, month, 1)
 end_date = Date.new(year, month, -1) 
   
 puts "#{year}年#{month}月".center(20)
-puts "日 月 火 水 木 金 土".center(10)
+puts "日 月 火 水 木 金 土"
 first_wday = start_date.wday
 print "   " * first_wday
   
 (start_date..end_date).each do |date|
   date_str = date.day.to_s.rjust(2,' ')
-
   print date_str + ' '
 
-    if 
-      date.saturday?
-      puts
-    end
-    
-    if
-      date == end_date
-      puts 
-    end
+  if date.saturday?
+     puts
+  end
 end
+
+puts
