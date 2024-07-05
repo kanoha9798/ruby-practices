f209c667877e7bd2bb5e09be17d47672dd52aba2
kanoha9798 <km.nick34@gmail.com>
2024-07-05 15:04:51 +0900

    一時保存

--- 03.bowling/bowling.rb
+++ 03.bowling/bowling.rb
@@ -13,8 +13,6 @@ scores.each_with_index do |s|
 end
 
 
-p shots
-
 frames = []
 i = 0
 while i < shots.length
@@ -27,26 +25,24 @@ else
   end
 end
 
-frames[-1].compact!
-
-p frames
-
 
 point = 0
 frames[0..9].each_with_index do |frame, index|
+    next_frame = frames[index + 1]|| [0, 0]
+    next_next_frame = frames[index + 2]|| [0, 0]
   if frame[0] == 10 # ストライク
-    if frames[index + 1][0]  && frames[index + 2][0]  && frames[index + 1][0]  == 10 # 連続連続ストライク
-        point += frame[0]  + frames[index + 1][0]  + frames[index + 2][0] 
-      elsif frames[index + 1][0]  && frames[index + 1][0]  == 10 # 連続ストライク
-        point += frame[0]  + frames[index + 1][0] 
-      elsif frames[index + 1] # 次のフレームがスペアまたは合計10未満
-        point += frame[0]  + frames[index + 1].sum
+    if next_frame[0]  && next_next_frame[0]  && next_frame[0]  == 10 # 連続連続ストライク
+        point += frame[0]  + next_frame[0]  + next_next_frame[0] 
+      elsif next_frame[0]  && next_frame[0]  == 10 # 連続ストライク
+        point += frame[0]  + next_frame[0] 
+      elsif next_frame # 次のフレームがスペアまたは合計10未満
+        point += frame[0]  + next_frame.sum
       else
         point += frame.sum
       end
   elsif frame.sum == 10 # スペア
-    if frames[index + 1]
-      point += frame.sum + frames[index + 1][0]
+    if next_frame
+      point += frame.sum + next_frame[0]
     else
       point += frame.sum
     end
